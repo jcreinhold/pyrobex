81926c604e909764f432d136a6ee81105894e884
Jacob Reinhold <jacob.reinhold@jhu.edu>
2021-05-06 18:30:52 -0400

    Bump version: 0.1.0 → 0.2.0

--- pyrobex/__init__.py
+++ pyrobex/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Jacob Reinhold"""
 __email__ = 'jcreinhold@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from pyrobex.pyrobex import robex, robex_mask, robex_stripped
 from pyrobex import errors
